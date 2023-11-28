01fba5ff01152957b246a142f67c9c08df8be7c9
junios <junios@naver.com>
2023-11-28 15:25:02 +0900

    사각형 출력

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
+
+#include <iostream>
 #include "SDL.h"
+
 
 #pragma comment(lib, "SDL2")
 #pragma comment(lib, "SDL2main")
 
 int SDL_main(int argc, char* argv[])
 {
+	srand((unsigned int)(time(nullptr)));
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window* MyWindow = SDL_CreateWindow("HelloWorld", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
 	
-	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
+	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
 	SDL_Event MyEvent;
 	bool IsRunning = true;
@@ -28,11 +33,24 @@ int SDL_main(int argc, char* argv[])
 			}
 			break;
 		}
-		//¸í·É¾î ÁË´Ù ÀúÀå
+		//º×
 		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0xff, 0xff, 0x00);
 		SDL_RenderClear(MyRenderer);
 
-//		SDL_RenderDrawRect()
+		for (int i = 0; i < 100000; ++i)
+		{
+			int X = rand() % 800;
+			int Y = rand() % 600;
+			int W = rand() % 200 + 10;
+			int H = rand() % 200 + 10;
+			int R = rand() % 256;
+			int G = rand() % 256;
+			int B = rand() % 256;
+			int A = rand() % 256;
+			SDL_SetRenderDrawColor(MyRenderer, R, G, B, A);
+			SDL_Rect MyRect = { X, Y, W, H };
+			SDL_RenderFillRect(MyRenderer, &MyRect);
+		}
 
 		SDL_RenderPresent(MyRenderer);
 	}
